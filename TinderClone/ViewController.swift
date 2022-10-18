d50a6a282a69471074badfd19c56272d1e0078be
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-10-18 11:23:18 -0400

    first commit

---
+++ TinderClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderClone
+//
+//  Created by Yaz Burrell on 10/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
