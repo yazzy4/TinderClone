4d84770e80c622bba1891edb145900972f6c6616
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-10-20 18:46:10 -0400

    added label for name and info button

--- TinderClone/View/CardView.swift
+++ TinderClone/View/CardView.swift
@@ -19,8 +19,29 @@ class CardView: UIView {
         return iv
     }()
     
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: "Jane Doe", attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy), .foregroundColor: UIColor.white])
+        
+        attributedText.append(NSAttributedString(string: "  20", attributes: [.font: UIFont.systemFont(ofSize: 24), .foregroundColor: UIColor.white]))
+        
+        label.attributedText = attributedText
+        
+        return label
+       
+    }()
     
-    // MARK: - Lifecycel
+    // always original keeps white color for button
+    private lazy var infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "info_icon")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    
+    // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,6 +52,14 @@ class CardView: UIView {
         
         addSubview(imageView)
         imageView.fillSuperview()
+        
+        addSubview(infoLabel)
+        infoLabel.anchor(left: leftAnchor, bottom:  bottomAnchor, right: rightAnchor, paddingLeft: 16, paddingBottom: 16, paddingRight: 16)
+        
+        addSubview(infoButton)
+        infoButton.setDimensions(height: 40, width: 40)
+        infoButton.centerY(inView: infoLabel)
+        infoButton.anchor(right: rightAnchor, paddingRight: 16)
     }
     
     required init?(coder: NSCoder) {
