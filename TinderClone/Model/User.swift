bee2d747093ab674eb57b370ffcb910c75314252
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-10-26 14:23:07 -0400

    added files for user model and card view model

---
+++ TinderClone/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  TinderClone
+//
+//  Created by Yaz Burrell on 10/26/22.
+//
+
+import Foundation
+
+struct User {
+    
+}
---
+++ TinderClone/ViewModel/CardViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CardViewModel.swift
+//  TinderClone
+//
+//  Created by Yaz Burrell on 10/26/22.
+//
+
+import Foundation
