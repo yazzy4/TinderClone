1863742d180e582724ad081015bb18f51581e8ad
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-11-01 12:57:27 -0400

    added files for login and registration

---
+++ TinderClone/Controller/Authentication/LoginController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginController.swift
+//  TinderClone
+//
+//  Created by Yaz Burrell on 11/1/22.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+}
---
+++ TinderClone/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,13 @@
+//
+//  RegistrationController.swift
+//  TinderClone
+//
+//  Created by Yaz Burrell on 11/1/22.
+//
+
+import UIKit
+
+
+class RegistrationController: UIViewController {
+    
+}
