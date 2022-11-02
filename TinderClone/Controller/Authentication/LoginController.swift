f3d8b46a57c3112ec9373c45eb06b0c7bf62fc4e
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-11-02 12:35:39 -0400

    setup email and password textfields in login controller. need to clean up

--- TinderClone/Controller/Authentication/LoginController.swift
+++ TinderClone/Controller/Authentication/LoginController.swift
@@ -17,6 +17,42 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private let emailTextField: UITextField = {
+        let tf = UITextField()
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        tf.leftView = spacer
+        tf.leftViewMode = .always
+        
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        tf.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        tf.layer.cornerRadius = 5
+        tf.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return tf
+        
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = UITextField()
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        tf.leftView = spacer
+        tf.leftViewMode = .always
+        
+        tf.borderStyle = .none
+        tf.textColor = .white
+        tf.backgroundColor = UIColor(white: 1, alpha: 0.2)
+        tf.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        tf.layer.cornerRadius = 5
+        tf.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return tf
+        
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -28,18 +64,26 @@ class LoginController: UIViewController {
     
     func configureUI() {
         navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
         configureGradient()
         
         view.addSubview(iconImageView)
         iconImageView.centerX(inView: view)
         iconImageView.setDimensions(height: 100, width: 100)
         iconImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField])
+        stack.axis = .vertical
+        stack.spacing = 16
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 24, paddingLeft: 32, paddingRight: 32)
     }
     
     func configureGradient() {
-        guard let topColor = UIColor(hex: "#FD297Bff") else { return }
-        guard let bottomColor = UIColor(hex: "#FF655Bff") else { return }
-        
+        guard let topColor = UIColor(hex: "#FF655Bff") else { return }
+        guard let bottomColor = UIColor(hex: "#FD297Bff") else { return }
+   
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
         gradientLayer.locations = [0, 1]
