3df7a6df94578d3a47421a68216214260e88473d
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-11-02 12:22:12 -0400

    completed color gradient for login screen

--- TinderClone/Controller/Authentication/LoginController.swift
+++ TinderClone/Controller/Authentication/LoginController.swift
@@ -8,5 +8,44 @@
 import UIKit
 
 class LoginController: UIViewController {
+    // MARK: - Properties
+    
+    private let iconImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "app_icon")?.withRenderingMode(.alwaysTemplate)
+        iv.tintColor = .white
+        return iv
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+        configureGradient()
+        
+        view.addSubview(iconImageView)
+        iconImageView.centerX(inView: view)
+        iconImageView.setDimensions(height: 100, width: 100)
+        iconImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+    }
+    
+    func configureGradient() {
+        guard let topColor = UIColor(hex: "#FD297Bff") else { return }
+        guard let bottomColor = UIColor(hex: "#FF655Bff") else { return }
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.locations = [0, 1]
+        view.layer.addSublayer(gradientLayer)
+        gradientLayer.frame = view.frame
+        
+    }
     
 }
--- TinderClone/Controller/Authentication/RegistrationController.swift
+++ TinderClone/Controller/Authentication/RegistrationController.swift
@@ -9,5 +9,11 @@ import UIKit
 
 
 class RegistrationController: UIViewController {
+    // MARK: - Properties
     
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
